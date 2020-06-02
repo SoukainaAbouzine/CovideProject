a53dcbe547c8d6cce1f652cddf61f4f122aa8b8e
Hanae <hanaelabiad@gmail.com>
2020-06-02 21:10:54 +0100

    init 02-06-2020

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { ContenuComponent } from './contenu/contenu.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ContenuComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
